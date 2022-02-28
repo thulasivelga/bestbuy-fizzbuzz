a6b27d2aeb777e49b2a9f44026540de6ba728651
Thulasi Velga <v.thulasireddy@gmail.com>
2022-02-27 22:27:16 -0500

    added outline examples

--- src/test/java/com/thulasi/assignments/betsbuy/FizzBuzzStepDef.java
+++ src/test/java/com/thulasi/assignments/betsbuy/FizzBuzzStepDef.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -29,20 +30,42 @@ public class FizzBuzzStepDef {
 
 	@When("user enters {input}")
 	public void getInput(String input) {
-		driver.findElement(By.id("")).sendKeys(input);
+		driver.findElement(By.id("textbox")).sendKeys(input); // Assuming input text box id is 'textbox'
+		driver.findElement(By.id("button")).click(); // Assuming click button id is 'button'
 	}
 
 	@Then("verify {column1} ,{column2} are displayed in the table")
 	public void verifyOutput(String column1, String column2) {
+		String input = driver.findElement(By.xpath("//*[@name='table']/tbody/tr[1]/td[0]")).getText();
+		String expectedOutput = driver.findElement(By.xpath("//*[@name='table']/tbody/tr[1]/td[0]")).getText();
+
+		Assert.assertEquals(input, column1);
+		Assert.assertEquals(expectedOutput, column2);
+
+		// TODO below code is to traverse the complete tables.
 		List<WebElement> rowElements = driver.findElements(By.xpath("//*[@name='table']/tbody/tr"));
 		int rowsize = rowElements.size();
 		List<WebElement> colElements = driver.findElements(By.xpath("//*[@name='table']/tbody/tr[1]/td"));
 		int colsize = colElements.size();
+
+		String[][] actualTableData = new String[rowsize][colsize];
 		for (int i = 1; i <= rowsize; i++) {
 			for (int j = 1; j <= colsize; j++) {
-				driver.findElement(By.xpath("//*[@name='table']/tbody/tr[" + i + "]/td[" + j + "]")).getText();
+				actualTableData[i - 1][j - 1] = driver
+						.findElement(By.xpath("//*[@name='table']/tbody/tr[" + i + "]/td[" + j + "]")).getText();
 			}
 		}
+
+		// TODO get this data from the selenium outline examples
+		String[][] expectedTableData = new String[rowsize][colsize];
+
+		// Comparing the 2 2-D arrays to be equal
+		for (int i = 0; i < rowsize; i++) {
+			for (int j = 0; j < colsize; j++) {
+				Assert.assertEquals(actualTableData[i][j], expectedTableData[i][j]);
+			}
+		}
+
 	}
 
 }
--- src/test/resources/fizzbuzz.feature
+++ src/test/resources/fizzbuzz.feature
@@ -6,14 +6,20 @@ Scenario Outline: validating FizzBuzz web application
     Examples: 
       | input      | column1     |      column2                           |  
  ############################################################################     
-      |  1         |      1      | divided 1 by 3, divided 1 by 5         | 
+      |  1         |      1      |divided 1 by 3,divided 1 by 5           | 
       |  3         |      3      |fizz                                    | 
       |  5         |      5      |buzz                                    |
       |  ""        |      empty  |invalid item                            |
       |  15        |      15     |fizzbuzz                                |
       |  a         |      a      |invalid item                            |
-      |  23        |      23     |divided 23 by 3, divided 23 by 5        |
-      
+      |  23        |      23     |divided 23 by 3,divided 23 by 5         |
+      |  @         |      @      |invalid item                            |
+      |  -1        |      -1     |invalid item                            |
+      |  abc       |      abc    |invalid item                            |
+      |  120       |      120    |fizzbuzz                                |
+      |  3a        |      3a     |invalid item                            |
+      |  1.9       |      1.9    |invalid item                            |
+         
       
        
 
